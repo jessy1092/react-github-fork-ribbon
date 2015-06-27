516c4dc0e9ceb858f4d203e78b95f07751d3dcc3
Tom Chen <developer@tomchentw.com>
2015-06-27 23:12:51 +0800

    Rewrite to remove dependency on react-style
    * Use named export/import for './ribbonStyle'
    * Remove unnecessary "use strict" for ES2015 modules
    * Use assign from 'react/lib/Object.assign' to create final style
    * Rename internal this.position to this.ribbonPosition to remove ambiguity on this.props.position

--- src/ribbonStyle.js
+++ src/ribbonStyle.js
@@ -1,168 +1,160 @@
 /*
- * "Fork me on GitHub" CSS ribbon v0.1.1 | MIT License
- * https://github.com/simonwhitaker/github-fork-ribbon-css
- *
- * 20150116 Lee: use css source for inline style react component.
+* "Fork me on GitHub" CSS ribbon v0.1.1 | MIT License
+* https://github.com/simonwhitaker/github-fork-ribbon-css
+*
+* 20150116 Lee: use css source for inline style react component.
 */
+export const GitHubForkRibbon = {
 
-"use strict";
+  baseStyle: {
+    /* The right and left classes determine the side we attach our banner to */
+    position: 'absolute',
 
-import StyleSheet from 'react-style';
+    /* Add a bit of padding to give some substance outside the "stitching" */
+    padding: '2px 0',
 
-export default {
+    /* Set the base colour */
+    backgroundColor: '#a00',
 
-  GitHubForkRibbon: StyleSheet.create({
+    /* Set a gradient: transparent black at the top to almost-transparent black at the bottom */
+    // backgroundImage: '-webkit-gradient(linear, left top, left bottom, from(rgba(0, 0, 0, 0)), to(rgba(0, 0, 0, 0.15)))',
+    // backgroundImage: '-webkit-linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.15))',
+    // backgroundImage: '-moz-linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.15))',
+    // backgroundImage: '-ms-linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.15))',
+    // backgroundImage: '-o-linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.15))',
+    backgroundImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.15))',
 
-    baseStyle: {
-      /* The right and left classes determine the side we attach our banner to */
-      position: 'absolute',
+    /* Add a drop shadow */
+    // -webkit-box-shadow: '0 2px 3px 0 rgba(0, 0, 0, 0.5)',
+    // -moz-box-shadow: '0 2px 3px 0 rgba(0, 0, 0, 0.5)',
+    boxShadow: '0 2px 3px 0 rgba(0, 0, 0, 0.5)',
 
-      /* Add a bit of padding to give some substance outside the "stitching" */
-      padding: '2px 0',
+    /* Set the font */
+    font: '700 13px "Helvetica Neue", Helvetica, Arial, sans-serif',
 
-      /* Set the base colour */
-      backgroundColor: '#a00',
+    zIndex: '9999',
+    pointerEvents: 'auto'
+  },
 
-      /* Set a gradient: transparent black at the top to almost-transparent black at the bottom */
-      // backgroundImage: '-webkit-gradient(linear, left top, left bottom, from(rgba(0, 0, 0, 0)), to(rgba(0, 0, 0, 0.15)))',
-      // backgroundImage: '-webkit-linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.15))',
-      // backgroundImage: '-moz-linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.15))',
-      // backgroundImage: '-ms-linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.15))',
-      // backgroundImage: '-o-linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.15))',
-      backgroundImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.15))',
+  urlStyle: {
+    /* Set the text properties */
+    color: '#fff',
+    textDecoration: 'none',
+    textShadow: '0 -1px rgba(0, 0, 0, 0.5)',
+    textAlign: 'center',
 
-      /* Add a drop shadow */
-      // -webkit-box-shadow: '0 2px 3px 0 rgba(0, 0, 0, 0.5)',
-      // -moz-box-shadow: '0 2px 3px 0 rgba(0, 0, 0, 0.5)',
-      boxShadow: '0 2px 3px 0 rgba(0, 0, 0, 0.5)',
+    /* Set the geometry. If you fiddle with these you'll also need
+       to tweak the top and right values in .github-fork-ribbon. */
+    width: '200px',
+    lineHeight: '20px',
 
-      /* Set the font */
-      font: '700 13px "Helvetica Neue", Helvetica, Arial, sans-serif',
+    /* Set the layout properties */
+    display: 'inline-block',
+    padding: '2px 0',
 
-      zIndex: '9999',
-      pointerEvents: 'auto'
-    },
+    /* Add "stitching" effect */
+    borderWidth: '1px 0',
+    borderStyle: 'dotted',
+    // borderColor: '#fff',
+    borderColor: 'rgba(255, 255, 255, 0.7)'
+  },
 
-    urlStyle: {
-      /* Set the text properties */
-      color: '#fff',
-      textDecoration: 'none',
-      textShadow: '0 -1px rgba(0, 0, 0, 0.5)',
-      textAlign: 'center',
+  redColor: {
+    backgroundColor: '#a00'
+  },
 
-      /* Set the geometry. If you fiddle with these you'll also need
-         to tweak the top and right values in .github-fork-ribbon. */
-      width: '200px',
-      lineHeight: '20px',
+  orangeColor: {
+    backgroundColor: '#f80'
+  },
 
-      /* Set the layout properties */
-      display: 'inline-block',
-      padding: '2px 0',
+  blackColor: {
+    backgroundColor: '#333'
+  },
 
-      /* Add "stitching" effect */
-      borderWidth: '1px 0',
-      borderStyle: 'dotted',
-      // borderColor: '#fff',
-      borderColor: 'rgba(255, 255, 255, 0.7)'
-    },
+  greenColor: {
+    backgroundColor: '#090'
+  },
 
-    redColor: {
-      backgroundColor: '#a00'
-    },
+  leftStyle: {
+    top: '42px',
+    left: '-43px',
 
-    orangeColor: {
-      backgroundColor: '#f80'
-    },
+    // -webkit-transform: 'rotate(-45deg)',
+    // -moz-transform: 'rotate(-45deg)',
+    // -ms-transform: 'rotate(-45deg)',
+    // -o-transform: 'rotate(-45deg)',
+    transform: 'rotate(-45deg)'
+  },
 
-    blackColor: {
-      backgroundColor: '#333'
-    },
+  rightStyle: {
+    top: '42px',
+    right: '-43px',
 
-    greenColor: {
-      backgroundColor: '#090'
-    },
+    // -webkit-transform: 'rotate(45deg)',
+    // -moz-transform: 'rotate(45deg)',
+    // -ms-transform: 'rotate(45deg)',
+    // -o-transform: 'rotate(45deg)',
+    transform: 'rotate(45deg)'
+  },
 
-    leftStyle: {
-      top: '42px',
-      left: '-43px',
+  leftBottomStyle: {
+    top: '80px',
+    left: '-43px',
 
-      // -webkit-transform: 'rotate(-45deg)',
-      // -moz-transform: 'rotate(-45deg)',
-      // -ms-transform: 'rotate(-45deg)',
-      // -o-transform: 'rotate(-45deg)',
-      transform: 'rotate(-45deg)'
-    },
+    // -webkit-transform: 'rotate(45deg)',
+    // -moz-transform: 'rotate(45deg)',
+    // -ms-transform: 'rotate(45deg)',
+    // -o-transform: 'rotate(45deg)',
+    transform: 'rotate(45deg)'
+  },
 
-    rightStyle: {
-      top: '42px',
-      right: '-43px',
+  rightBottomStyle: {
+    top: '80px',
+    right: '-43px',
 
-      // -webkit-transform: 'rotate(45deg)',
-      // -moz-transform: 'rotate(45deg)',
-      // -ms-transform: 'rotate(45deg)',
-      // -o-transform: 'rotate(45deg)',
-      transform: 'rotate(45deg)'
-    },
+    // -webkit-transform: 'rotate(-45deg)',
+    // -moz-transform: 'rotate(-45deg)',
+    // -ms-transform: 'rotate(-45deg)',
+    // -o-transform: 'rotate(-45deg)',
+    transform: 'rotate(-45deg)'
+  }
+};
 
-    leftBottomStyle: {
-      top: '80px',
-      left: '-43px',
+export const GitHubForkRibbonWrapper = {
 
-      // -webkit-transform: 'rotate(45deg)',
-      // -moz-transform: 'rotate(45deg)',
-      // -ms-transform: 'rotate(45deg)',
-      // -o-transform: 'rotate(45deg)',
-      transform: 'rotate(45deg)'
-    },
+  baseStyle: {
+    width: '150px',
+    height: '150px',
+    position: 'absolute',
+    overflow: 'hidden',
+    top: '0',
+    zIndex: '9999',
+    pointerEvents: 'none'
+  },
 
-    rightBottomStyle: {
-      top: '80px',
-      right: '-43px',
+  fixedStyle: {
+    position: 'fixed'
+  },
 
-      // -webkit-transform: 'rotate(-45deg)',
-      // -moz-transform: 'rotate(-45deg)',
-      // -ms-transform: 'rotate(-45deg)',
-      // -o-transform: 'rotate(-45deg)',
-      transform: 'rotate(-45deg)'
-    }
-  }),
+  leftStyle: {
+    left: '0'
+  },
 
-  GitHubForkRibbonWrapper: StyleSheet.create({
+  rightStyle: {
+    right: '0'
+  },
 
-    baseStyle: {
-      width: '150px',
-      height: '150px',
-      position: 'absolute',
-      overflow: 'hidden',
-      top: '0',
-      zIndex: '9999',
-      pointerEvents: 'none'
-    },
+  leftBottomStyle: {
+    position: 'fixed',
+    top: 'inherit',
+    bottom: '0',
+    left: '0'
+  },
 
-    fixedStyle: {
-      position: 'fixed'
-    },
-
-    leftStyle: {
-      left: '0'
-    },
-
-    rightStyle: {
-      right: '0'
-    },
-
-    leftBottomStyle: {
-      position: 'fixed',
-      top: 'inherit',
-      bottom: '0',
-      left: '0'
-    },
-
-    rightBottomStyle: {
-      position: 'fixed',
-      top: 'inherit',
-      bottom: '0',
-      right: '0'
-    }
-  })
+  rightBottomStyle: {
+    position: 'fixed',
+    top: 'inherit',
+    bottom: '0',
+    right: '0'
+  }
 };
