6bd2f9b4f67a1a0c44282460f96e50d8659f1904
Lee <jessy1092@gmail.com>
2017-10-05 16:50:51 +0800

    test: Separate the test case into folder on storybook layout

--- storybook/__tests__/GithubForkRibbon.stories.js
+++ storybook/__tests__/GithubForkRibbon.stories.js
@@ -4,114 +4,107 @@ import { storiesOf, describe, it, specs } from '../facade';
 
 import GitHubForkRibbon from '../../src';
 
-const stories = storiesOf('GitHub fork ribbon', module);
+storiesOf('GitHub fork ribbon/default', module)
+  .add('default with right position and red color', () => {
+    const component = (
+      <GitHubForkRibbon href="//www.google.com" target="_blank">
+        Fork me on GitHub
+      </GitHubForkRibbon>
+    );
 
-stories.add('default with right position and red color', () => {
-  const component = (
-    <GitHubForkRibbon href="//www.google.com" target="_blank">
-      Fork me on GitHub
-    </GitHubForkRibbon>
-  );
+    return component;
+  })
+  .add('handle unexist position with default', () => {
+    const component = (
+      <GitHubForkRibbon href="//www.google.com" target="_blank" position="WOW">
+        Fork me on GitHub
+      </GitHubForkRibbon>
+    );
 
-  return component;
-});
+    return component;
+  })
+  .add('handle unexist color with default', () => {
+    const component = (
+      <GitHubForkRibbon href="//www.google.com" target="_blank" color="Hi">
+        Fork me on GitHub
+      </GitHubForkRibbon>
+    );
 
-stories.add('handle unexist position with default', () => {
-  const component = (
-    <GitHubForkRibbon href="//www.google.com" target="_blank" position="WOW">
-      Fork me on GitHub
-    </GitHubForkRibbon>
-  );
+    return component;
+  });
 
-  return component;
-});
+storiesOf('GitHub fork ribbon/position', module)
+  .add('on right position', () => {
+    const component = (
+      <GitHubForkRibbon href="//www.google.com" target="_blank" position="right">
+        Fork me on GitHub
+      </GitHubForkRibbon>
+    );
 
-stories.add('handle unexist color with default', () => {
-  const component = (
-    <GitHubForkRibbon href="//www.google.com" target="_blank" color="Hi">
-      Fork me on GitHub
-    </GitHubForkRibbon>
-  );
+    return component;
+  })
+  .add('on left position', () => {
+    const component = (
+      <GitHubForkRibbon href="//www.google.com" target="_blank" position="left">
+        Fork me on GitHub
+      </GitHubForkRibbon>
+    );
 
-  return component;
-});
+    return component;
+  })
+  .add('on right-bottom position', () => {
+    const component = (
+      <GitHubForkRibbon href="//www.google.com" target="_blank" position="right-bottom">
+        Fork me on GitHub
+      </GitHubForkRibbon>
+    );
 
-stories.add('on right position', () => {
-  const component = (
-    <GitHubForkRibbon href="//www.google.com" target="_blank" position="right">
-      Fork me on GitHub
-    </GitHubForkRibbon>
-  );
+    return component;
+  })
+  .add('on left-bottom position', () => {
+    const component = (
+      <GitHubForkRibbon href="//www.google.com" target="_blank" position="left-bottom">
+        Fork me on GitHub
+      </GitHubForkRibbon>
+    );
 
-  return component;
-});
+    return component;
+  });
 
-stories.add('on left position', () => {
-  const component = (
-    <GitHubForkRibbon href="//www.google.com" target="_blank" position="left">
-      Fork me on GitHub
-    </GitHubForkRibbon>
-  );
+storiesOf('GitHub fork ribbon/color', module)
+  .add('with red color', () => {
+    const component = (
+      <GitHubForkRibbon href="//www.google.com" target="_blank" color="red">
+        Fork me on GitHub
+      </GitHubForkRibbon>
+    );
 
-  return component;
-});
+    return component;
+  })
+  .add('with orange color', () => {
+    const component = (
+      <GitHubForkRibbon href="//www.google.com" target="_blank" color="orange">
+        Fork me on GitHub
+      </GitHubForkRibbon>
+    );
 
-stories.add('on right-bottom position', () => {
-  const component = (
-    <GitHubForkRibbon href="//www.google.com" target="_blank" position="right-bottom">
-      Fork me on GitHub
-    </GitHubForkRibbon>
-  );
+    return component;
+  })
+  .add('with black color', () => {
+    const component = (
+      <GitHubForkRibbon href="//www.google.com" target="_blank" color="black">
+        Fork me on GitHub
+      </GitHubForkRibbon>
+    );
 
-  return component;
-});
+    return component;
+  })
+  .add('with green color', () => {
+    const component = (
+      <GitHubForkRibbon href="//www.google.com" target="_blank" color="green">
+        Fork me on GitHub
+      </GitHubForkRibbon>
+    );
 
-stories.add('on left-bottom position', () => {
-  const component = (
-    <GitHubForkRibbon href="//www.google.com" target="_blank" position="left-bottom">
-      Fork me on GitHub
-    </GitHubForkRibbon>
-  );
-
-  return component;
-});
-
-stories.add('with red color', () => {
-  const component = (
-    <GitHubForkRibbon href="//www.google.com" target="_blank" color="red">
-      Fork me on GitHub
-    </GitHubForkRibbon>
-  );
-
-  return component;
-});
-
-stories.add('with orange color', () => {
-  const component = (
-    <GitHubForkRibbon href="//www.google.com" target="_blank" color="orange">
-      Fork me on GitHub
-    </GitHubForkRibbon>
-  );
-
-  return component;
-});
-
-stories.add('with black color', () => {
-  const component = (
-    <GitHubForkRibbon href="//www.google.com" target="_blank" color="black">
-      Fork me on GitHub
-    </GitHubForkRibbon>
-  );
-
-  return component;
-});
-
-stories.add('with green color', () => {
-  const component = (
-    <GitHubForkRibbon href="//www.google.com" target="_blank" color="green">
-      Fork me on GitHub
-    </GitHubForkRibbon>
-  );
-
-  return component;
-});
+    return component;
+  });
